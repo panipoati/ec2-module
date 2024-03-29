2af99fa236ca9b2302063275b081d588029c2dfb
Pani <poatipani@gmail.com>
2024-03-28 19:24:56 -0500

    terraform

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "Amazon machine image"
+  type = string
+  default = "ami-033a1ebf088e56e81"
+}
+variable "instance_type" {
+  default = "t2.small"
+}
+variable "region" {
+#  default = "us-east-1"
+}
+variable "profile" {
+  default = "default"
+}
